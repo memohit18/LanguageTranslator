d55a4e300da3e338e91b9d1dce7beb67f2b06fa3
= <=>
2024-03-18 03:12:57 +0530

    commenting

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.get('/', function (req,res) {
     res.send({"Info": "This is Home Page"})
 })
 
-
 //Post API for translation 
 app.post('/translate', function (req, res) {
     return TranslateEngToFrench.getTranslation(req,res)
 })
 
+//Post API to get the minimum Cost to deliver 
 app.post('/getcost',function (req,res) {
     return MinimumCostController.getMinimumCost(req,res)
 })
